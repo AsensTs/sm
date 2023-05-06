225034f107482888f5660645ecc900f91111d6a5
asensts <asensts@163.com>
2023-05-06 13:31:58 +0800

    update

---
+++ sm-coupon/src/main/java/com/as/sm/coupon/SmCouponApplication.java
@@ -0,0 +1,13 @@
+package com.as.sm.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmCouponApplication.class, args);
+    }
+
+}
---
+++ sm-member/src/main/java/com/as/sm/member/SmMemberApplication.java
@@ -0,0 +1,13 @@
+package com.as.sm.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmMemberApplication.class, args);
+    }
+
+}
