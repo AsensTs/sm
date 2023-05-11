5bd877d04a9ee4460a17dd18bd87c7f803b6f449
asensts <asensts@163.com>
2023-05-11 20:35:40 +0800

    update

---
+++ cloud/src/test/java/com/as/cloud/CloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.as.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
