7b6bf4eec43aeb0dcdd665fac845aa373c75c36c
tsh <asensts@163.com>
2023-07-26 17:47:45 +0800

    update login page

--- react-next-app/src/app/layout.tsx
+++ react-next-app/src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
     <html lang="HTML5">
       <body className={inter.className}>
--- react-next-app/src/pages/login/index.tsx
+++ react-next-app/src/pages/login/index.tsx
@@ -1,11 +1,12 @@
-import "./login.scss"
 import "@/common/styles/global.scss"
+import loginStyle from "./login.module.scss"
 import { useRouter } from "next/router";
 import http from "@/common/utils/http.js"
-import { Button, Checkbox, Form, Input, message } from 'antd';
-import { useEffect } from "react";
+import { Button, Checkbox, Form, Input, message, Modal } from 'antd';
+import { useState } from "react";
 
 
+// 登录确认
 const onFinish = async (values: any, router: any) => {
     try {
         let res = await http.post("/api/login", values);
@@ -18,26 +19,45 @@ const onFinish = async (values: any, router: any) => {
     }
 };
 
+// 提交表单且数据验证失败后回调事件
 const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
 };
 
+const registerUser = (values: any) => {
+
+}
+
+
 const Login: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     
     const router = useRouter();
     let wrapperCol = {
         offset: 6,
         span: 14
     }
+
+    // 打开注册窗口
+    const onRegister = () => {
+        setIsModalOpen(true);
+    }
+
+    // 注册确认并关闭窗口
+    const onRegisterFinish = (values: any) => {
+        setIsModalOpen(true);
+        registerUser(values)
+    }
+
     return (
-        <div className="login_page">
+        <div className={loginStyle.login_page}>
             <div>
-                <div className="login_title">
-                    <p className="title">登录界面</p>
+                <div className={loginStyle.login_title}>
+                    <p className={loginStyle.title}>登录界面</p>
                 </div>
-                <div className="login_form">
+                <div className={loginStyle.login_form}>
                     <Form
-                        className="form"
+                        className={loginStyle.form}
                         name="basic"
                         labelCol={{ span: wrapperCol.offset }}
                         wrapperCol={{ span: wrapperCol.span }}
@@ -47,7 +67,7 @@ const Login: React.FC = () => {
                         autoComplete="off"
                     >
                         <Form.Item
-                            label="用户名"
+                            label="账 号"
                             name="username"
                             rules={[{ required: true, message: 'Please input your username!' }]}
                         >
@@ -67,12 +87,38 @@ const Login: React.FC = () => {
                         </Form.Item>
 
                         <Form.Item wrapperCol={wrapperCol}>
-                            <Button type="primary" htmlType="submit">
-                                登录
-                            </Button>
+                            <Button type="primary" htmlType="submit">登录</Button>
+                            <Button type="default" onClick={onRegister} style={{marginLeft: '10px'}}>注册</Button>
                         </Form.Item>
                     </Form>
                 </div>
+
+                <Modal title="注册账号" open={isModalOpen} onCancel={() => setIsModalOpen(false)} footer={[]}>
+                    <div className={loginStyle.register_modal} >
+                        <Form
+                            name="nest-messages"
+                            onFinish={onRegisterFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item label="账 号" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+                                <Input/>
+                            </Form.Item>
+                            <Form.Item label="密 码" name="password" rules={[{ required: true },{ type: 'string', min: 6 }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="邮 箱" name="email" rules={[{ required: true },{ type: 'string' }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item label="手机号码" name="mobile" rules={[{ required: true },{ type: 'string', min: 11 }]}>
+                                <Input />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="default">清空</Button>
+                                <Button type="primary" htmlType="submit">提交</Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </Modal>
             </div>
         </div>
     )
